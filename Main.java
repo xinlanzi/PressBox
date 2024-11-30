91d6be9392add1826d36bf228257310e770f3ec8
xinlanzi <12411755@mail.sustech.edu.cn>
2024-11-30 17:42:50 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,11 +6,11 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            LoginFrame loginFrame = new LoginFrame(280,280);
+            LoginFrame loginFrame = new LoginFrame(600,400);
             loginFrame.setVisible(true);
             LevelFrame levelFrame = new LevelFrame(500,200);
-            levelFrame.setVisible(false);
-            loginFrame.setLevelFrame(levelFrame);
+            levelFrame.setVisible(false);//等到第一个页面关闭才显现
+            loginFrame.setLevelFrame(levelFrame);//展示关卡
         });
     }
 }
